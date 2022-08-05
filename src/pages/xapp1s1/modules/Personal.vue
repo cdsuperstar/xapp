aa48b1dc6f9bed57cbdbb16337d277fa3abc021f
1017266402@qq.com <tang.yuanjie@student.zy.cdut.edu.cn>
2022-08-05 19:03:16 +0800

    Personal page draft.

--- src/pages/xapp1s1/modules/Personal.vue
+++ src/pages/xapp1s1/modules/Personal.vue
@@ -1,5 +1,70 @@
 <template>
-  <q-page class="flex flex-center"> </q-page>
+  <q-page>
+    <q-avatar
+      size="100px"
+      font-size="52px"
+      color="teal"
+      text-color="white"
+      icon="directions"
+    />
+    <q-form class="q-gutter-md">
+      <q-input
+        filled
+        v-model="name"
+        label="Your name *"
+        hint="Name and surname"
+        lazy-rules
+        :rules="[(val) => (val && val.length > 0) || 'Please type something']"
+      />
+      <q-input
+        filled
+        v-model="name"
+        label="Your name *"
+        hint="Name and surname"
+        lazy-rules
+        :rules="[(val) => (val && val.length > 0) || 'Please type something']"
+      />
+      <q-input
+        filled
+        v-model="name"
+        label="Your name *"
+        hint="Name and surname"
+        lazy-rules
+        :rules="[(val) => (val && val.length > 0) || 'Please type something']"
+      />
+      <q-input
+        filled
+        v-model="name"
+        label="Your name *"
+        hint="Name and surname"
+        lazy-rules
+        :rules="[(val) => (val && val.length > 0) || 'Please type something']"
+      />
+    </q-form>
+
+    <q-list bordered padding>
+      <q-item-label header>User Controls</q-item-label>
+
+      <q-item clickable v-ripple>
+        <q-item-section>
+          <q-item-label>Content filtering</q-item-label>
+          <q-item-label caption>
+            Set the content filtering level to restrict apps that can be
+            downloaded
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-item clickable v-ripple>
+        <q-item-section>
+          <q-item-label>Password</q-item-label>
+          <q-item-label caption>
+            Require password for purchase or use password to restrict purchase
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-page>
 </template>
 
 <script>
