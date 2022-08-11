8b72d5b9492ffa2d55478d9069ef35a4f92b45a9
AntaresOvO <1422810622@qq.com>
2022-08-11 08:33:52 +0000

    modified shop info page

--- src/pages/xapp1s1/modules/Shop.vue
+++ src/pages/xapp1s1/modules/Shop.vue
@@ -1,65 +1,78 @@
 <template>
   <q-page>
+    <q-toolbar>
+      <q-btn flat round dense icon="keyboard_arrow_left" to="/user/xapp1s1personal"/>
+      <q-toolbar-title>
+        <label class="title">
+            {{ $t("xapp1s1.shop.header") }}
+        </label>
+      </q-toolbar-title>
+    </q-toolbar>
     <q-form class="q-gutter-md">
-      <q-input
-        v-model="myShop.name"
-        :label="$t('xapp1s1.shop.name')"
-      />
-      <q-input v-model="myShop.starttime" mask="time" :rules="['time']" :hint="$t('xapp1s1.shop.starttime')">
-        <template v-slot:append>
-          <q-icon name="access_time" class="cursor-pointer">
-            <q-popup-proxy transition-show="scale" transition-hide="scale">
-              <q-time v-model="myShop.starttime">
-                <div class="row items-center justify-end">
-                  <q-btn v-close-popup label="Close" color="primary" flat />
-                </div>
-              </q-time>
-            </q-popup-proxy>
-          </q-icon>
-        </template>
-      </q-input>
-      <q-input v-model="myShop.endtime" mask="time" :rules="['time']" :hint="$t('xapp1s1.shop.endtime')">
-        <template v-slot:append>
-          <q-icon name="access_time" class="cursor-pointer">
-            <q-popup-proxy transition-show="scale" transition-hide="scale">
-              <q-time v-model="myShop.endtime">
-                <div class="row items-center justify-end">
-                  <q-btn v-close-popup label="Close" color="primary" flat />
-                </div>
-              </q-time>
-            </q-popup-proxy>
-          </q-icon>
-        </template>
-      </q-input>
-      <q-select v-model="myShop.status" :options="options" :label="$t('xapp1s1.shop.status')" />
-      <q-input
-        v-model="myShop.phone"
-        :label="$t('xapp1s1.shop.phone')"
-      />
-      <q-input
-        v-model="myShop.tel"
-        :label="$t('xapp1s1.shop.tel')"
-      />
-      <q-input
-        v-model="myShop.addr"
-        :label="$t('xapp1s1.shop.addr')"
-      />
-      <q-btn
-        color="deep-orange"
-        glossy
-        :label="$t('xapp1s1.shop.ok')"
-        @click="update"
-      />
-      <q-btn
-        color="blue"
-        glossy
-        :label="$t('buttons.back')"
-        to="/user/xapp1s1personal"
-      />
+
+      <q-input class="col-6 q-pa-md" v-model="myShop.name" :label="$t('xapp1s1.shop.name')"
+        :rules="[val => val.length <= 30 || '请不要超过30个字符']" outlined rounded />
+
+      <div class="row q-pa-md">
+        <q-input v-model="myShop.starttime" mask="time" :rules="['time']" :hint="$t('xapp1s1.shop.starttime')"
+          class="col-6" outlined rounded style="padding-right: 8px">
+          <template v-slot:append>
+            <q-icon name="access_time" class="cursor-pointer">
+              <q-popup-proxy transition-show="scale" transition-hide="scale">
+                <q-time v-model="myShop.starttime">
+                  <div class="row items-center justify-end">
+                    <q-btn v-close-popup label="Close" color="primary" flat />
+                  </div>
+                </q-time>
+              </q-popup-proxy>
+            </q-icon>
+          </template>
+        </q-input>
+
+
+        <q-input v-model="myShop.endtime" mask="time" :rules="['time']" :hint="$t('xapp1s1.shop.endtime')" class="col-6"
+          outlined rounded>
+          <template v-slot:append>
+            <q-icon name="access_time" class="cursor-pointer">
+              <q-popup-proxy transition-show="scale" transition-hide="scale">
+                <q-time v-model="myShop.endtime">
+                  <div class="row items-center justify-end">
+                    <q-btn v-close-popup label="Close" color="primary" flat />
+                  </div>
+                </q-time>
+              </q-popup-proxy>
+            </q-icon>
+          </template>
+        </q-input>
+      </div>
+
+
+
+      <q-select v-model="myShop.status" :options="options" :label="$t('xapp1s1.shop.status')" class="col-6 q-pa-md"
+        outlined rounded />
+
+
+      <q-input v-model="myShop.phone" :label="$t('xapp1s1.shop.phone')" class="col-6 q-pa-md" outlined rounded />
+
+
+      <q-input v-model="myShop.tel" :label="$t('xapp1s1.shop.tel')" class="col-6 q-pa-md" outlined rounded />
+
+
+      <q-input v-model="myShop.addr" :label="$t('xapp1s1.shop.addr')" class="col-6 q-pa-md" outlined rounded />
+
+
+
+      <div class="row q-pa-md buttons">
+        <q-btn push color="savebtn" :label="$t('xapp1s1.shop.ok')" @click="update" />
+      </div>
+
     </q-form>
+
+
     <q-inner-loading :showing="loading">
       <q-spinner-box size="50px" color="secondary" />
     </q-inner-loading>
+
   </q-page>
 </template>
 
@@ -73,7 +86,7 @@ export default {
       startTime: '09:00',
       endTime: '22:00',
       myShop: {},
-      options: [this.$t('xapp1s1.shop.working'),this. $t('xapp1s1.shop.rest')],
+      options: [this.$t('xapp1s1.shop.working'), this.$t('xapp1s1.shop.rest')],
     };
   },
   created() {
@@ -83,19 +96,19 @@ export default {
       if (res.data.success === true) {
         this.myShop = res.data.data;
         this.loading = false;
-      }else{
+      } else {
         this.myShop = {
-          name:null,
+          name: null,
           starttime: '09:00',
           endtime: '22:00',
-          status:null,
-          phone:null,
-          tel:null,
-          addr:null,
-          longitude:null,
-          latitude:null,
+          status: null,
+          phone: null,
+          tel: null,
+          addr: null,
+          longitude: null,
+          latitude: null,
         }
-        console.log(typeof(this.myShop));
+        console.log(typeof (this.myShop));
         this.loading = false;
       }
       //this.myShop = res.data.data;
@@ -128,4 +141,8 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.buttons {
+  justify-content: space-around;
+}
+</style>
