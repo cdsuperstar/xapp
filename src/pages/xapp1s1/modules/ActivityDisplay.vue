85da14e4546ddac36163b0e5a336e47cc940466f
LQQ <481897250@qq.com>
2023-08-27 13:32:05 +0800

    update ActivitDisplay

--- src/pages/xapp1s1/modules/ActivityDisplay.vue
+++ src/pages/xapp1s1/modules/ActivityDisplay.vue
@@ -1,126 +1,108 @@
 <template>
   <q-page>
 <div class="row justify-center items-center">
-    <div class="column q-gutter-y-md   justify-center items-center col-xs-12 col-sm-8 col-md-6" >
-        <!-- <q-footer reveal elevated>
-        <q-toolbar>
-
-          <q-toolbar-title>
-            <strong>Quasar</strong> Framework
-          </q-toolbar-title>
-          
-        </q-toolbar>
-      </q-footer> -->
-<!-- 粘性 -->
+    <div class="column q-gutter-y-md   justify-center items-center col-xs-12 col-sm-8 col-md-6" style="background-color:  rgb(238, 238, 238);">
 
 
 
-    <div class="shadow-1 justify-center items-center" style="max-width: auto; width: 100%">
-        <div style="margin: 10px;"><strong><h4 > {{ activity }}</h4> </strong> </div>
+<!-- 活动名字及部分内容 -->
+    <div class=" justify-center items-center shadow-1" style="max-width: auto; width: 100%;background-color: white;">
+        <div class="text-bold" style="font-size: xx-large;padding: 10px">{{ activity }}</div>
                 <div>
                      <q-item clickable v-ripple>
                     <q-item-section side>
                         <q-avatar rounded size="48px" style="padding: 0px;">
                         <img src="https://cdn.quasar.dev/img/avatar.png" />
-                        <q-badge floating color="teal">new</q-badge>
                         </q-avatar>
                     </q-item-section>
                     <q-item-section >
-                        <q-item-label caption style="padding: 0px;">Hosted by</q-item-label>
-                        <q-item-label style="padding: 0px;">Mary</q-item-label>
+                        <q-item-label caption style="padding: 0px;">主办方</q-item-label>
+                        <q-item-label style="padding: 0px;">某某</q-item-label>
                     </q-item-section>
                     </q-item> 
-                 </div> 
+                </div> 
     </div>
 
-    <q-separator />
-
-    <div class=" q-px-lg q-pt-sm justify-center items-center" style="max-width: auto; width: 100%"> 
-        <q-separator />
-        <div>举办内容:xxxxxxxxxxxxxxxxx</div>
-        <div>举办方式:XXXXXXXXXXXXXXXXX</div>
-        <div>小组内容:xxxxxxxxxxxxxxxxx</div>
-        <div>具体内容:xxxxxxxxxxxxxxxxx</div>
-        <q-separator />
+    
+<!-- 活动的具体内容-->
+    <div class="  q-pt-sm justify-center items-center shadow-1" style="max-width: auto; width: 100%;background-color: white;"> 
+        <div style="margin: 15px;"><q-avatar color="red" text-color="white" icon="directions" size="sm"/>举办时间:2023.12.12</div>
+        <div style="margin: 15px;margin-bottom: 20px;"><q-avatar color="red" text-color="white" icon="directions" size="sm"/>举办方式:线上举办</div>
+        <q-item clickable v-ripple style="margin-bottom: 5px;">
+                <q-item-section side>
+                    <q-avatar rounded size="48px">
+                    <img src="https://cdn.quasar.dev/img/avatar.png" />
+                    <!-- <q-badge floating color="teal">new</q-badge> -->
+                    </q-avatar>
+                </q-item-section>
+                <q-item-section>
+                    <q-item-label>小组名</q-item-label>
+                    <q-item-label caption>有关小组更多信息</q-item-label>
+                </q-item-section>
+            </q-item>
+                <q-card class="my-card no-shadow">
+                <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+                    <div class="absolute-bottom text-subtitle2 text-center">
+                    Title
+                    </div>
+                </q-img>
+                </q-card>
+            <div>
+                <!-- 此处写活动的具体内容 --><div class="text-center">活动具体内容</div>
+            </div>
     </div> 
     
         <q-separator />
-        <div class=" q-px-lg q-pt-lg justify-center items-center" style="max-width: auto; width: 100%">Attendees(n) 
-            
+<!-- 参加者列表 -->
+        <div class=" q-px-md q-pt-lg  justify-center items-center" style="max-width: auto; width: 100%;">
+            <div class="text-bold" style="font-size: large;">
+                参加者
+            </div>
             <q-virtual-scroll
                 :items="heavyList"
                 virtual-scroll-horizontal
                 v-slot="{ item, index }"
                 virtual-scroll-slice-size="30"
-                > 
-                                           
-                            <!-- <q-card style="width: 300px;"
-                                        :key="index">
-                            <q-img src="https://cdn.quasar.dev/img/parallax2.jpg"  >
-                                <div class="absolute-bottom text-h6">
-                                {{item.title}}
-                                </div>
-                            </q-img>
-                            <q-card-section>
-                                {{ lorem }}
-                            </q-card-section>
-                            </q-card> -->
+                >                    
 
-                            <q-card
-                                class="my-card2"
-                                style="margin: 5px"
-                                :key="index"
-                            >
-                                                <q-card-section horizontal>
-                                                    <q-img
-                                                        class="col-5"
-                                                        src="https://cdn.quasar.dev/img/parallax1.jpg"
-                                                    />
-
-                                                    <q-card-section>
-                                                        {{ item.title }}
-                                                    </q-card-section>
-                                                </q-card-section>
-
-                                                <q-separator />
-
-                                                <q-card-actions>
-                                                    <q-btn
-                                                        flat
-                                                        round
-                                                        icon="event"
-                                                    />
-                                                    <q-btn flat> 5:30PM </q-btn>
-                                                    <q-btn flat> 7:00PM </q-btn>
-                                                    <q-btn
-                                                        flat
-                                                        color="primary"
-                                                    >
-                                                        Reserve
-                                                    </q-btn>
-                                                </q-card-actions>
-                                                </q-card>
-
-
+                        <q-card style="margin: 5px; max-width: 300px;height: 200px;" :key="index">
+                        <q-item>
+                            <q-item-section class="item-center justify-center">
+                            
+                                <img src="https://cdn.quasar.dev/img/avatar2.jpg" style="width: 100%;height: 70px;">
+                            
+                            </q-item-section>
+                        </q-item>
+                    <q-item>
+                            <q-item-section>
+                            <q-item-label class="text-center">{{ item.member }}</q-item-label>
+                            <q-item-label caption class="text-center">成员职位</q-item-label>
+                            </q-item-section>
+                        </q-item>
+                        </q-card>
 
             </q-virtual-scroll>
-            
+
             <p style="text-align:right;">
                 <a href="">
                     更多
                 </a>
             </p>
         </div>
-
-        <div class=" q-px-lg q-pt-lg justify-center items-center" style="max-width: auto; width: 100%"> Photos(n) 
-                    <div>
-                            
-                            <q-card class="my-card" >
-                                <q-card-section>
-                                    {{ lorem }}
-                                </q-card-section>
+<!-- 图片列表 -->
+        <div class=" q-px-md q-pt-lg justify-center items-center " style="max-width: auto; width: 100%;">
+                    <div class="text-bold" style="font-size: large;">
+                    照片集
+                    </div>
+            
+                    <div> 
+                            <q-card class="my-card">
+                            <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+                                <div class="absolute-bottom text-subtitle2 text-center">
+                                Title
+                                </div>
+                            </q-img>
                             </q-card>
-
                     </div>
                         <p style="text-align:right;">
                             <a href="">
@@ -128,12 +110,12 @@
                             </a>
                         </p>
         </div>
-
-        <div class="col q-px-lg q-pt-lg justify-center items-center" style="max-width: auto; width: 100%">
-                <q-card class="my-card">
+<!-- 小组卡片 -->
+        <div class=" q-pt-lg" style="max-width: auto; width: 100%;margin-bottom: 30px;">
+                <q-card class="my-card no-shadow">
                 <q-card-section class="bg-primary text-white">
-                    <div class="text-h6">group name</div>
-                    <div class="text-subtitle2">by John Doe</div>
+                    <div class="text-h6">小组名</div>
+                    <div class="text-subtitle2">组织者:某某</div>
                 </q-card-section>
 
                 <q-separator />
@@ -145,8 +127,10 @@
                 </q-card>
         </div>
         <q-separator />
-        <div class="q-px-lg q-pt-lg justify-center items-center" style="max-width: auto; width: 100%">
-            similar evnets nearby
+        <div class="q-px-md q-pt-lg justify-center items-center shadow-1" style="max-width: auto; width: 100%;">
+            <div class="text-bold" style="font-size: large;">
+            附近的活动
+            </div>
                         <q-virtual-scroll
                             :items="heavyList"
                             virtual-scroll-horizontal
@@ -205,7 +189,7 @@
 const maxSize = 5
 const heavyList = []
 for (let i = 0; i < maxSize; i++){
-heavyList.push({title:"test"});
+heavyList.push({title:"测试",member:"成员名称"});
 }
 export default {
     data(){
@@ -230,6 +214,7 @@ export default {
 //   padding: 10px 15px
 .my-card
   width: 100%
+  margin-top: 15px
 .my-card1
     width: 30%
     height: 95%
